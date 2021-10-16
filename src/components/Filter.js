2abaf9e9afd5dcb7394fc9fd17ecde0efd3bbb84
AshishBhalala11 <ashishbhalala2911@gmail.com>
2021-10-16 21:30:34 +0530

    second

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,6 +3,8 @@ import React,{useState} from 'react';
 // const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=3a94078fb34b772a31d9a1348035bed7&query=";
 const MOVIE_API = "https://api.themoviedb.org/3/discover/movie?api_key=3a94078fb34b772a31d9a1348035bed7";
 const SERIES_API = "https://api.themoviedb.org/3/tv/popular?api_key=3a94078fb34b772a31d9a1348035bed7";
+const GENRE_API = "https://api.themoviedb.org/3/discover/movie?api_key=3a94078fb34b772a31d9a1348035bed7&language=en-US&sort_by=popularity.desc&with_genres="
+const RELEASE_YEAR_API = "https://api.themoviedb.org/3/discover/movie?api_key=3a94078fb34b772a31d9a1348035bed7&language=en-US&sort_by=popularity.desc&primary_release_year="
 
 const Filter = (props) =>{
     const yearArray = ['2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021'];
@@ -14,20 +16,20 @@ const Filter = (props) =>{
         
     }
     const genreChange = (e) => {
-        
+        //console.log(e.target.value);
+        const genreUrl = GENRE_API+e.target.value;
+        fetchMovie(genreUrl);
     }
-    const fromYearChange = (e) => {
-        
-    }
-    const toYearChange = (e) => {
-        
+    const yearChange = (e) => {
+        const yearUrl = RELEASE_YEAR_API + e.target.value;
+        fetchMovie(yearUrl);
     }
 
     const fetchMovie = (API) => {
         fetch(API)
             .then(res => res.json())
             .then(data => {
-                console.log(data.results);                
+                //console.log(data.results);                
                 props.search(data.results);
             })
     }
@@ -35,7 +37,7 @@ const Filter = (props) =>{
         fetch(API)
             .then(res => res.json())
             .then(data => {
-                console.log(data.results);                
+                //console.log(data.results);                
                 props.filter(data.results);
             })
     }
@@ -51,19 +53,18 @@ const Filter = (props) =>{
             </select>
             <div className="lable">Genre</div>
             <select id="genre" className="form-select selection" aria-label="Default select example" onChange={genreChange}>
-                <option value="Action">Action</option>
-                <option value="Drama">Drama</option>
-                <option value="Comedy">Comedy</option>
-                <option value="Thriller">Thriller</option>
-                <option value="Sci-Fi">Sci-Fi</option>
+                <option value={`${28}`}>Action</option>
+                <option value={`${18}`}>Drama</option>
+                <option value={`${35}`}>Comedy</option>
+                <option value={`${53}`}>Thriller</option>
+                <option value={`${878}`}>Sci-Fi</option>
+                <option value={`${27}`}>Horror</option>
+                <option value={`${10749}`}>Romance</option>
+                <option value={`${14}`}>Fantasy</option>
+                <option value={`${10751}`}>Family</option>
             </select>
             <div className="lable">Year</div>
-            <select id="from-year" className="form-select selection" aria-label="Default select example" onChange={fromYearChange}>
-                {yearArray.map((year) => {
-                    return <option key={year} value={`${year}`}>{year}</option>
-                })}
-            </select>
-            <select id="to-year" className="form-select selection" aria-label="Default select example" onChange={toYearChange}>
+            <select id="year" className="form-select selection" aria-label="Default select example" onChange={yearChange}>
                 {yearArray.map((year) => {
                     return <option key={year} value={`${year}`}>{year}</option>
                 })}
